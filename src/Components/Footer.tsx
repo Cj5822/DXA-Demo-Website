306f682c04f49f0e47bbcf128df12ecdf9a2cfdd
ljun123@aucklanduni.ac.nz <ljun123@aucklanduni.ac.nz>
2026-01-02 13:52:40 +1300

    Changed the footer to be sticky

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -7,9 +7,13 @@ const Footer = () => {
       sx={{
         borderTop: 1,
         borderColor: "divider",
-        mt: 6,
         bgcolor: "grey.900",
         color: "grey.100",
+        position: "fixed",
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 1300,
       }}
     >
       <Box
